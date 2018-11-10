f65f75d589435d5caf01a673b7ce391b2cc24f98
rmania <dmeijerink1979@gmail.com>
2018-11-10 22:19:04 +0100

    decision boundary plot plus

--- ml_models/helper_functions/ml_visuals.py
+++ ml_models/helper_functions/ml_visuals.py
@@ -65,29 +65,38 @@ def pprint_confusion_matrix(confusion_matrix, class_names, figsize = (10,7), fon
 
 
 
-def plot_decision_boundaries(X, y, classifier, test_idx=None, resolution=0.02):
+def plot_decision_boundaries(X, y, classifier, test_idx=None, h=0.02):
     
     fig, ax = plt.subplots(1,1, figsize=(15,8))
     
     # setup marker generator and color map
     markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    colors = ('red', 'blue', 'darkgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
 
     # plot decision surface
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                           np.arange(x2_min, x2_max, resolution))
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    ax.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
-    ax.set(xlim = (xx1.min(), xx1.max()), ylim = (xx2.min(), xx2.max()))
-
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    
+    Z = classifier.predict(np.array([xx.ravel(), yy.ravel()]).T)
+    Z = Z.reshape(xx.shape)
+    ax.contourf(xx, yy, Z, alpha=0.4, cmap=plt.cm.jet)
+    ax.set(xlim = (xx.min(), xx.max()), ylim = (yy.min(), yy.max()), title=classifier)
+    
+    ax.set_title(classifier)
+    
+    # print classifier score
+    score = classifier.score(X,y)
+    ax.text(xx.max() - .3, yy.min() + .3, 
+           ('mean accuracy : %.3f' % score).lstrip('0'),
+            size=15, horizontalalignment='right')
+        
     for idx, cl in enumerate(np.unique(y)):
         ax.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
-                    alpha=0.8, c=cmap(idx),
-                    marker=markers[idx], label=cl)
+                   alpha=0.8, c=cmap(idx),
+                   marker=markers[idx], label=cl)
 
     # highlight test samples
     if test_idx:
