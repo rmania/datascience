ba571082ca7b676975e277a0783a22366c10212e
rmania <dmeijerink1979@gmail.com>
2018-11-11 21:24:28 +0100

    wip

--- ml_models/helper_functions/data_manipulation.py
+++ ml_models/helper_functions/data_manipulation.py
@@ -66,3 +66,15 @@ def make_diagonal(x):
     for i in range(len(m[0])):
         m[i, i] = x[i]
     return m
+
+def calculate_covariance_matrix(X, Y=None):
+    """ 
+    Calculate the covariance matrix for the dataset X 
+    """
+    if Y is None:
+        Y = X
+    n_samples = np.shape(X)[0]
+    covariance_matrix = (1 / (n_samples-1)) * (X - X.mean(axis=0)).T.dot(Y - Y.mean(axis=0))
+
+    return np.array(covariance_matrix, dtype=float)
+
