da425a787230ff30bdac18b2be8ec622f6b138b6
rmania <dmeijerink1979@gmail.com>
2018-11-13 14:16:38 +0100

    for study purposes

--- ml_models/04_2_dim_reduc_lda_recipes.py
+++ ml_models/04_2_dim_reduc_lda_recipes.py
@@ -1,4 +1,9 @@
 import numpy as np
+import sys
+sys.path.insert(0, 'helper_functions/')
+from data_manipulation import calculate_covariance_matrix
+
+## --- https://sebastianraschka.com/notebooks/ml-notebooks.html
 
 def compute_mean_vectors(X, y):
     """
@@ -67,3 +72,44 @@ def get_components(eig_vals, eig_vecs, n_comp=2):
     W = np.hstack([eig_pairs[i][1].reshape(4, 1) for i in range(0, n_comp)])
     
     return W
+
+## -----------------
+class LDA():
+    """
+    source: https://github.com/eriklindernoren/ML-From-Scratch
+    """
+    def __init__(self):
+        self.w = None
+
+    def transform(self, X, y):
+        self.fit(X, y)
+        # Project data onto vector
+        X_transform = X.dot(self.w)
+        return X_transform
+
+    def fit(self, X, y):
+        # Separate data by class
+        X1 = X[y == 0]
+        X2 = X[y == 1]
+
+        # Calculate the covariance matrices of the two datasets
+        cov1 = calculate_covariance_matrix(X1)
+        cov2 = calculate_covariance_matrix(X2)
+        cov_tot = cov1 + cov2
+
+        # Calculate the mean of the two datasets
+        mean1 = X1.mean(0)
+        mean2 = X2.mean(0)
+        mean_diff = np.atleast_1d(mean1 - mean2)
+
+        # Determine the vector which when X is projected onto it best separates the
+        # data by class. w = (mean1 - mean2) / (cov1 + cov2)
+        self.w = np.linalg.pinv(cov_tot).dot(mean_diff)
+
+    def predict(self, X):
+        y_pred = []
+        for sample in X:
+            h = sample.dot(self.w)
+            y = 1 * (h < 0)
+            y_pred.append(y)
+        return y_pred
